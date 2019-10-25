2f2245a8230a9e36c161356b6d87da2f98c4c3da
“Priyal” <priyal.mittal@gmail.com>
2019-10-25 04:29:45 -0400

    Added Home and Navbar components to add fixed navbar and improve routing

--- src/RouterComponent.js
+++ src/RouterComponent.js
@@ -2,13 +2,17 @@ import React from 'react'
 import {BrowserRouter as Router ,Route} from 'react-router-dom'
 import Entity from './containers/Entity'
 import EditEntity from './components/EditEntity'
+import Login from './components/Login'
+import Home from "./containers/Home";
 
 const RouterComponent = () =>{
     return (
         <Router>
-            <Route path="/entities"><Entity/></Route>
-            <Route path="/edit"><EditEntity/></Route>
-            <Route path='/new'><EditEntity/></Route>
+            <Route path='/' component={Home}/>
+            <Route path='/entities' component={Entity}/>
+            <Route path='/edit' component={EditEntity}/>
+            <Route path='/new' component={EditEntity}/>
+            <Route path='/login' component={Login}/>
         </Router>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import RouterComponent from './RouterComponent';
+import RouterComponent from "./RouterComponent";
 
-ReactDOM.render(<RouterComponent />, document.getElementById('root'));
+ReactDOM.render(<RouterComponent/>, document.getElementById('root'));
