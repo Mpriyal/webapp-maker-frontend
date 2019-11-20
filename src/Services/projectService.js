e4bebcd783ee48b5acf3b1d42ff6a837f2d6a779
“Priyal” <priyal.mittal@gmail.com>
2019-11-20 16:19:07 -0500

    Added createProjectForUser service to project services file

---
+++ src/Services/projectService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+const API_URL = "http://localhost:4000/";
+
+const createProjectForUser = async (userId, project) => {
+    let projectRes = await axios.post(API_URL + 'api/user/' + userId + '/project', project);
+    return projectRes
+};
+
+export {createProjectForUser}
